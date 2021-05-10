5103268fc8fa6e91a1c3bf19fd518a883f7189ca
marusho <mrsho34@gmail.com>
2021-05-10 21:57:34 +0900

    feat: 引数の取得部分を実装

--- task_14/src/get-numbers.js
+++ task_14/src/get-numbers.js
@@ -1,6 +1,13 @@
 const getNumbers = (args) => {
-  //pass
-};
+  if (!args.length || args.length > 30)
+		throw "Invalid Number of arguments";
 
+  const nums = args.map(Number);
+
+  if (nums.some(isNaN))
+    throw "Not a Number exists in arguments";
+
+    return nums;
+};
 
 module.exports = getNumbers;
