1ffdeddbf3ad29df2112b29b4c4a6cbc86b1f2f2
marusho <mrsho34@gmail.com>
2021-05-10 19:14:46 +0900

    fix: export module

--- task_14/src/get-operator.js
+++ task_14/src/get-operator.js
@@ -1,14 +1,16 @@
-module.exports = (args) => {
+const getOperator = (args) => {
   if (!isOperator(args[0]))
      throw "Invalid operator";
   return args[0];
 };
 
-let isOperator = (arg) => {
-  return (
-    arg === "add" ||
-    arg === "subtract" ||
-    arg === "multiply" ||
-    arg === "divide"
-  );
+const isOperator = (arg) => {
+	return (
+		arg === "add" ||
+		arg === "subtract" ||
+		arg === "multiply" ||
+		arg === "divide"
+	);
 };
+
+module.exports = getOperator;
