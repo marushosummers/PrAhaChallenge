a4aedc98c3b5c9c4f0572df47be7c135f7617608
marusho <mrsho34@gmail.com>
2021-05-10 22:37:50 +0900

    feat: 足し算を実装

--- task_14/src/add.js
+++ task_14/src/add.js
@@ -1,5 +1,6 @@
 const add = (nums) => {
-  // pass
+  const sum = nums.reduce((accumulator, current) => accumulator + current, 0);
+  return sum <= 1000 ? sum : "too big";
 };
 
 module.exports = add;
